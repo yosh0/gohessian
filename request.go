23be80a560b369b2de3e758e1a3f08faecf7b118
HackeZ <767110505@qq.com>
2016-11-06 13:36:05 +0800

    add hessian client

--- request.go
+++ request.go
@@ -1,77 +1,87 @@
 package gohessian
 
 import (
-  "bytes"
-  "io"
-  "net/http"
-  "fmt"
-  "io/ioutil"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 )
 
 type hessian_request struct {
-  body []byte
+	body []byte
 }
 
-//向hessian服务发请求,并将解析结果返回
-//url string hessian 服务地址
-//method string hessian 公开的方法
-//params ...Any 请求参数
-func Request(url string, method string, params ...Any) (interface{}, error) {
-  r := &hessian_request{}
-  r.pack_head(method)
-  for _, v := range params {
-    r.pack_param(v)
-  }
-  r.pack_end()
-  
-  resp, err := http_post(url, bytes.NewReader(r.body))
-  if err != nil {
-    return nil, err
-  }
-  
-  h := NewHessian(bytes.NewReader(resp))
-  v, err := h.Parse()
-  
-  if err != nil {
-    return nil, err
-  }
-
-  return v, nil
+// NewClient return a client of hessian
+// host string
+// url  string
+func NewClient(host, url string) (c Client) {
+	return &Client{
+		Host: host,
+		URL:  url,
+	}
 }
 
-//http post 请求,返回body字节数组
-func http_post(url string, body io.Reader) (rb []byte, err error) {
-  var resp *http.Response
-  if resp, err = http.Post(url, "application/binary", body); err != nil {
-    return nil, err
-  }
-  if resp.StatusCode != http.StatusOK {
-    err = fmt.Errorf(resp.Status)
-    return
-  }
-  defer resp.Body.Close()
-  rb, err = ioutil.ReadAll(resp.Body)
-  return
+// Invoke send a request to hessian service and return the result of response
+// method string  => hessian service method
+// params ...Any  => request param
+func (c *Client) Invoke(method string, params ...Any) (interface{}, error) {
+	reqURL := c.Host + c.URL
+	r := &hessian_request{}
+	r.packHead(method)
+	for _, v := range params {
+		r.packParam(v)
+	}
+	r.packEnd()
+
+	resp, err := httpPost(reqURL, bytes.NewReader(r.body))
+	if err != nil {
+		return nil, err
+	}
+
+	h := NewHessian(bytes.NewReader(resp))
+	v, err := h.Parse()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return v, nil
 }
 
-//封装  hessian 请求头
-func (h *hessian_request) pack_head(method string) {
-  tmp_b, _ := PackUint16(uint16(len(method)))
-  h.body = append(h.body, []byte{99, 0, 1, 109}...)
-  h.body = append(h.body, tmp_b...)
-  h.body = append(h.body, []byte(method)...)
+//httpPost send HTTP POST request, return bytes in body
+func httpPost(url string, body io.Reader) (rb []byte, err error) {
+	var resp *http.Response
+	if resp, err = http.Post(url, "application/binary", body); err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf(resp.Status)
+		return
+	}
+	defer resp.Body.Close()
+	rb, err = ioutil.ReadAll(resp.Body)
+	return
 }
 
-//封装参数
-func (h *hessian_request) pack_param(p Any) {
-  tmp_b, err := Encode(p)
-  if err != nil {
-    panic(err)
-  }
-  h.body = append(h.body, tmp_b...)
+// packHead pack hessian request head
+func (h *hessian_request) packHead(method string) {
+	tmp_b, _ := PackUint16(uint16(len(method)))
+	h.body = append(h.body, []byte{99, 0, 1, 109}...)
+	h.body = append(h.body, tmp_b...)
+	h.body = append(h.body, []byte(method)...)
 }
 
-//封装包尾
-func (h *hessian_request) pack_end() {
-  h.body = append(h.body, 'z')
+// packParam pack param in hessian request
+func (h *hessian_request) packParam(p Any) {
+	tmp_b, err := Encode(p)
+	if err != nil {
+		panic(err)
+	}
+	h.body = append(h.body, tmp_b...)
+}
+
+// packEnd pack end of hessian request
+func (h *hessian_request) packEnd() {
+	h.body = append(h.body, 'z')
 }
